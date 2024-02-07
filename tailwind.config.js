ba75fd9da20a46d934586ff8319c9e7538392e79
claire <yue.katou@gmail.com>
2024-02-07 09:54:51 -0800

    adds tailwind color palette to app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        50: "#fff1f2",
+        100: "#ffe4e6",
+        200: "#fecdd3",
+        300: "#fda4af",
+        400: "#fb7185",
+        500: "#f43f5e",
+        600: "#e11d48",
+        700: "#be123c",
+        800: "#9f1239",
+        900: "#881337",
+        950: "#4c0519",
+      },
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      orange: colors.orange,
+      pink: colors.pink,
+      sky: colors.sky,
+      yellow: colors.yellow,
+    },
   },
   plugins: [],
 };
