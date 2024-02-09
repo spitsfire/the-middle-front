43e1d44bc911a14e794f6816889983e0cdbb326e
claire <yue.katou@gmail.com>
2024-02-09 12:28:19 -0800

    creates initial components for itinerary page

---
+++ src/components/results/Itinerary.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Itinerary = () => {
+  return <div>Itinerary</div>;
+};
+
+export default Itinerary;
---
+++ src/components/results/Map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div id='map-container' className='bg-gray-600'>
+      Map
+    </div>
+  );
+};
+
+export default Map;
