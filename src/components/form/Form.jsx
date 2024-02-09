3844c363fef744ecc30024510dedd1e01a2cc4f9
claire <yue.katou@gmail.com>
2024-02-09 11:51:31 -0800

    moves main tag from App to Form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -5,14 +5,16 @@ import ActivitiesFilter from "./ActivitiesFilter";
 
 const Form = () => {
   return (
-    <form className='py-2 px-2 container mx-auto w-3/4'>
-      <StartingPoints />
-      <GroupName />
-      <ActivitiesFilter />
-      <button type='submit' className='btn bg-primary-500 text-white mt-10'>
-        Find a Midpoint
-      </button>
-    </form>
+    <main className='mt-28'>
+      <form className='py-2 px-2 container mx-auto w-3/4'>
+        <StartingPoints />
+        <GroupName />
+        <ActivitiesFilter />
+        <button type='submit' className='btn bg-primary-500 text-white mt-10'>
+          Find a Midpoint
+        </button>
+      </form>
+    </main>
   );
 };
 
